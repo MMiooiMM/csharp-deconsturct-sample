2c63a1fbcacaaf92555faeb9a0290260ddcc754b
Mio <mio093611@gmail.com>
2021-05-15 13:37:40 +0800

    增加日期解構元組方法，並驗證內容。

--- DeconsturctDemo/Program.cs
+++ DeconsturctDemo/Program.cs
@@ -2,11 +2,42 @@
 
 namespace DeconsturctDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var (year, month, day) = DateTime.Now;
+            Log(year, month, day);
+
+            (year, month, day) = new DateTime(2021, 05, 16);
+            Log(year, month, day);
+
+            _ = Console.ReadKey();
+        }
+
+        static void Log(int year, int month, int day)
+        {
+            Console.WriteLine($"Year = {year}, Month = {month}, Day = {day}.");
+        }
+    }
+
+    internal static class DateTimeExtension
+    {
+        /// <summary>
+        /// 將日期型別解構成三個數字，分別為年、月、日。
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <param name="day"></param>
+        public static void Deconstruct(this DateTime dateTime,
+              out int year,
+              out int month,
+              out int day)
+        {
+            year = dateTime.Year;
+            month = dateTime.Month;
+            day = dateTime.Day;
         }
     }
 }
