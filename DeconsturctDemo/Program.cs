79da4020a10d0759910ea26b4f24ddd4732211b7
Mio <mio093611@gmail.com>
2021-05-15 13:29:21 +0800

    加入專案檔案。

---
+++ DeconsturctDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeconsturctDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
